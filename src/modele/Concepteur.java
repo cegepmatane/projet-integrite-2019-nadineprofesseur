99936466d64796bc89fb04a14377ce6fa2454310
nadineprofesseur <accentweb.com@gmail.com>
2019-08-27 17:07:11 -0400

    Donnee liee

---
+++ src/modele/Concepteur.java
@@ -0,0 +1,35 @@
+package modele;
+
+public class Concepteur {
+	
+	protected String nom;
+	protected String surnom;
+	protected String specialite;
+	protected String courriel;
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getSurnom() {
+		return surnom;
+	}
+	public void setSurnom(String surnom) {
+		this.surnom = surnom;
+	}
+	public String getSpecialite() {
+		return specialite;
+	}
+	public void setSpecialite(String specialite) {
+		this.specialite = specialite;
+	}
+	public String getCourriel() {
+		return courriel;
+	}
+	public void setCourriel(String courriel) {
+		this.courriel = courriel;
+	}
+
+}
